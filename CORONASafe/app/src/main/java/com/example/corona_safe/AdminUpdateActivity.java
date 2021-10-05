4eac127cbdfa7ede3d1d028ddd2b1057e31d2f50
FazBilla <86349694+FazBilla@users.noreply.github.com>
2021-10-05 11:35:26 +0530

    error fixed

--- CORONASafe/app/src/main/java/com/example/corona_safe/AdminUpdateActivity.java
+++ CORONASafe/app/src/main/java/com/example/corona_safe/AdminUpdateActivity.java
@@ -15,9 +15,9 @@ import static android.widget.Toast.LENGTH_SHORT;
 
 public class AdminUpdateActivity extends AppCompatActivity {
 
-    EditText editNIC, editUser, editEmail, editVacc, editVacST;
+    EditText editNICUP, editUserUP, editEmailUP, editVaccUP, editVacSTUP;
     SearchEvent searchUser;
-    Button btnV0,btnV1,btnV2,btnV3, btnAddnew;
+    Button btnV0,btnV1,btnV2,btnV3, btnUpdateUP, btndeleteUP;
     CustomerDBHelper CustomersDB;
     String editVacst = null;
 
@@ -26,28 +26,29 @@ public class AdminUpdateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adminupdate);
 
-        editNIC = (EditText) findViewById(R.id.editNIC);
-        editUser = (EditText) findViewById(R.id.editUser);
-        editEmail = (EditText) findViewById(R.id.editEmail);
-        editVacc = (EditText) findViewById(R.id.editVacc);
-        editVacST = (EditText) findViewById(R.id.editVacST);
+        editNICUP = (EditText) findViewById(R.id.editNICUP);
+        editUserUP = (EditText) findViewById(R.id.editUserUP);
+        editEmailUP = (EditText) findViewById(R.id.editEmailUP);
+        editVaccUP = (EditText) findViewById(R.id.editVaccUP);
+        editVacSTUP = (EditText) findViewById(R.id.editVacSTUP);
         CustomersDB = new CustomerDBHelper(this);
-        btnAddnew = (Button) findViewById(R.id.btnAddnew);
+        btnUpdateUP = (Button) findViewById(R.id.btnupdateUP);
+        btndeleteUP = (Button) findViewById(R.id.btndeleteUP);
         //btnV0 = (Button) findViewById(R.id.btnV0);
         // btnV1 = (Button) findViewById(R.id.btnV1);
         // btnV2 = (Button) findViewById(R.id.btnV2);
         // btnV3 = (Button) findViewById(R.id.btnV3);
 
 
-        btnAddnew.setOnClickListener(new View.OnClickListener() {
+        btnUpdateUP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String user = editUser.getText().toString();
-                String mail = editEmail.getText().toString();
-                String nic = editNIC.getText().toString();
-                String vacc = editVacc.getText().toString();
-                Integer  vacst = Integer.valueOf(editVacST.getText().toString());
+                String user = editUserUP.getText().toString();
+                String mail = editEmailUP.getText().toString();
+                String nic = editNICUP.getText().toString();
+                String vacc = editVaccUP.getText().toString();
+                Integer  vacst = Integer.valueOf(editVacSTUP.getText().toString());
 
                 if(user.equals("")||mail.equals("")||nic.equals("")||vacc.equals(""))
                     Toast.makeText(AdminUpdateActivity.this, "Please enter all the fields", LENGTH_SHORT).show();
